cc166edaf19701dbdf0ac8a285f5777fe4d87e48
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-08-29 20:12:03 +0530

    cleaning data

--- rollcall3.py
+++ rollcall3.py
@@ -1,6 +1,11 @@
-import os
-from dotenv import load_dotenv
+import pandas as pd
 
-load_dotenv()
-clientid=os.getenv("CLIENT_ID")
-clientsecret=os.getenv("CLIENT_SECRET")
+dforg=pd.read_csv("sample.csv")
+dforg["Start Time"]=pd.to_datetime(dforg["Start Time"])
+dforg["End Time"]=pd.to_datetime(dforg["End Time"])
+dforg["Session Duration"]=(dforg["End Time"]-dforg["Start Time"])
+dfmain=dforg.drop(["Join Time","Leave Time","Guest","Meeting ID","Topic","Start Time","End Time"],axis=1) #dropping uncessesary datas; Axis=1-coloumn
+ 
+print(dfmain.info())
+print(dfmain)
+'''print((dfmain["Start Time"]-dfmain["End Time"]))'''
