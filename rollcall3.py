ca3f1be4234a1b563db489c462c75090bd469003
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-08-29 20:17:11 +0530

    comments added

--- rollcall3.py
+++ rollcall3.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 dforg=pd.read_csv("sample.csv")
+#converting str->datetime
 dforg["Start Time"]=pd.to_datetime(dforg["Start Time"])
 dforg["End Time"]=pd.to_datetime(dforg["End Time"])
-dforg["Session Duration"]=(dforg["End Time"]-dforg["Start Time"])
+dforg["Session Duration"]=(dforg["End Time"]-dforg["Start Time"])   #calculating meeting duration
 dfmain=dforg.drop(["Join Time","Leave Time","Guest","Meeting ID","Topic","Start Time","End Time"],axis=1) #dropping uncessesary datas; Axis=1-coloumn
- 
+
 print(dfmain.info())
 print(dfmain)
-'''print((dfmain["Start Time"]-dfmain["End Time"]))'''
