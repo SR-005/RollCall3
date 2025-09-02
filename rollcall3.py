2d7c517ea87ef59a73e7746fd696fd79990c0893
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-09-02 13:57:57 +0530

    Created a POAP Event using API

--- rollcall3.py
+++ rollcall3.py
@@ -1,5 +1,13 @@
 import pandas as pd
 import numpy as np
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+API_KEY = os.getenv("POAP_API_KEY")
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
 dforg=pd.read_csv("sample.csv")
 #converting str->datetime
@@ -19,3 +27,35 @@ dfmain = dfmain.dropna()    #dropping and rows with null values
 
 present_users = dfmain[dfmain["Status"] == "Present"]["User Email"].tolist()    #made the emails of present students into a list
 print(present_users)
+
+url = "https://api.poap.tech/events"
+files = {
+    "image": (
+        "Screenshot%202025-08-06%20123712.png",
+        open("C:/Users/sreer/Downloads/Screenshot 2025-08-06 123712.png", "rb"),
+        "image/png"
+    )
+}
+payload = {
+    "virtual_event": "false",
+    "event_template_id": "1",
+    "private_event": "false",
+    "notify_issuer": "true",
+    "name": "Example Event 202",
+    "description": "This is an example event",
+    "city": "Buenos Aires",
+    "country": "Argentina",
+    "start_date": "10-09-2025",
+    "end_date": "12-09-2025",
+    "expiry_date": "12-09-2025",
+    "event_url": "https://poap.xyz",
+    "email": "test@example.com",
+    "secret_code": "234789"
+}
+headers = {
+    "accept": "application/json",
+    "x-api-key": API_KEY
+}
+response = requests.post(url, data=payload, files=files, headers=headers)
+print(response.text)
+
