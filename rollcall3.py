5aed5b45610e463d52d34ecf04def9400fe60be0
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-09-02 23:29:28 +0530

    resolving api issues

--- rollcall3.py
+++ rollcall3.py
@@ -38,7 +38,7 @@ print(present_users)
 
 #----------------------------------------------------------------------API SECTION----------------------------------------------------------------------
 
-#Creating Event through API Requests
+'''#Creating Event through API Requests
 url = "https://api.poap.tech/events"    
 #Image for Badge logo
 files = {
@@ -69,7 +69,7 @@ headers = {
     "x-api-key": API_KEY
 }
 response = requests.post(url, data=payload, files=files, headers=headers)
-print(response.text)
+print(response.text)'''
 
 #Verifying Event Creationg - Data Fetching
 url = "https://api.poap.tech/events/id/200582"
@@ -78,4 +78,43 @@ headers = {
     "x-api-key": API_KEY
 }
 response = requests.get(url, headers=headers)
+print(response.text)
+
+'''url = f"https://api.poap.tech/events/200582/mint-links"
+headers = {
+    "x-api-key": API_KEY,
+    "accept": "application/json",
+    "content-type": "application/json"
+}
+
+payload = {
+    "secret_code": "example-secret-2025",   # choose any random string
+    "email": "test@example.com",       # issuer email
+    "links_count": 20                       # number of claim links you want
+}
+
+response = requests.post(url, headers=headers, json=payload)
+
+if response.status_code == 200:
+    data = response.json()
+    print("✅ Mint Links Generated:")
+    for link in data.get("links", []):
+        print(link)
+else:
+    print("❌ Error:", response.status_code, response.text)'''
+
+
+
+url = "https://api.poap.tech/event/200582/qr-codes"
+
+payload = { "secret_code": "234789" }
+headers = {
+    "accept": "application/json",
+    "content-type": "application/json",
+    "authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjExQVB4Sy0tMjdnVUlSVFhGaXFtTSJ9.eyJpc3MiOiJodHRwczovL2Rldi0yamEzd3ZrdGQ1NGszbW5uLnVzLmF1dGgwLmNvbS8iLCJzdWIiOiJkYjFpcEpuekd4THRPVEdzbzhGazhNUjFnZWhJOVhwS0BjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9yb2xsY2FsbDMuYXBpIiwiaWF0IjoxNzU2ODIzMDExLCJleHAiOjE3NTY5MDk0MTEsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyIsImF6cCI6ImRiMWlwSm56R3hMdE9UR3NvOEZrOE1SMWdlaEk5WHBLIn0.sKldG5_QaZj3MHQCnA4-lVKf3LlpnBnaoKxPXSN7YK4KZNTN2OHUs1ALzjPW57LDI2cz0dAU2Ezm8-h7FlSbN8Ct3_HudsV1TvnKoteWDk_JNc2xsb4FFnkr7BE_OWrdIIvgi-scGl4jp1356bsgbMClh72nLej692W5Xpu1Lqn2_LgVS7tgC9Cf4q11eefxYk8BQoH2Y38WNoWb5McyHncEGYIahE-J8gkc_CcBU0lRKvMMxkSLsgYihcoEVBSsFxin9U9cvtkTGB6lejxeuBwbWbebCUU378ltB9JkSkNPhsmddqKhTkGfbG_KnXO3Qk-vyabGC06jZrfH5AlqeQ",
+    "x-api-key": "w0OvficGb8l5sJ2SaDANHQpPGhhsyiIhJ9eBcoOQGKrxRhfGYdO4PgstV5yczmuz8v8w9JlwpIvXRt52dABFpwUegEbJEmDpNJBxwoE9t7lXfCcn00szniKzdlj7NJ0B"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
 print(response.text)
